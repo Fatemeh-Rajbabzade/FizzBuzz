e0df8adb59c51dd43349458d29abc4260b2159d0
Fatemeh Rajabzade <ra.fatemeh851@gmail.com>
2025-03-25 13:10:05 -0700

    Add Condition class

--- src/Condition.java
+++ src/Condition.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Condition {
+    // عددی که باید بر ان بخش پذیر باشه توی هر سری
+    int divisor;
+    // رشته دلخواه مان
+    String output;
+
+    public Condition(int divisor, String output) {
+        this.divisor = divisor;
+        this.output = output;
+    }
 }
